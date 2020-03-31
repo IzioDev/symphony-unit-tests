d1b388714cab896cdcad29ec26e4570d68ab04a0
Romain Billot <romainbillot3009@gmail.com>
2020-03-31 16:41:16 +0200

    unit_test(path)

--- tests/Entity/PathTest.php
+++ tests/Entity/PathTest.php
@@ -4,16 +4,29 @@
 namespace App\Tests\Entity;
 
 
+use App\Entity\Location;
 use App\Entity\Path;
 use PHPUnit\Framework\TestCase;
 
 class PathTest extends TestCase
 {
     protected $path;
+    protected $location1;
+    protected $location2;
 
     public function setUp(): void
     {
         $this->path = new Path();
+
+        $this->location1 = new Location();
+        $this->location1->setName("Lyon");
+        $this->location1->setLon(10.0);
+        $this->location1->setLat(10.0);
+
+        $this->location2 = new Location();
+        $this->location2->setName("Annecy");
+        $this->location2->setLon(10.0);
+        $this->location2->setLat(10.0);
     }
 
     public function testShouldBeInstanceOfPath()
@@ -34,6 +47,27 @@ class PathTest extends TestCase
         $this->path->setStartTime($dateTime);
 
         $this->assertSame($this->path->getStartTime(), $dateTime);
+    }
+
+    public function testShouldSetStartLocation()
+    {
+        $this->path->setStartLocation($this->location1);
+
+        $this->assertSame($this->path->getStartLocation(), $this->location1);
+    }
+
+    public function testShouldSetEndLocation()
+    {
+        $this->path->setEndLocation($this->location2);
+
+        $this->assertSame($this->path->getEndLocation(), $this->location2);
+    }
+
+    public function testShould()
+    {
+        $this->path->setLeftSeats(4);
+
+        $this->assertSame($this->path->getLeftSeats(), 4);
     }
 
     public function testIdShouldBeNull()
