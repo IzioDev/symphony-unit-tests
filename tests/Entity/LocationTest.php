fc74dddc1aab182cd061a71340896a6f94747e46
Quentin <quentin.somveille3874@gmail.com>
2020-04-01 19:25:10 +0200

    feat(location): tests unitaires

--- tests/Entity/LocationTest.php
+++ tests/Entity/LocationTest.php
@@ -6,13 +6,19 @@ namespace App\Tests\Entity;
 
 use App\Entity\Location;
 use PHPUnit\Framework\TestCase;
+use App\Entity\Path;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 
 class LocationTest extends TestCase
 {
     protected $location;
+    protected $path;
     public function setUp(): void
     {
         $this->location = new Location();
+        $this->path = new Path();
     }
 
     public function testShouldBeInstanceOfLocation()
@@ -44,5 +50,41 @@ class LocationTest extends TestCase
     public function testIdShouldBeNull()
     {
         $this->assertNull($this->location->getId());
+    }
+    
+    public function testShouldSetStartPaths() {
+        $this->assertInstanceOf(ArrayCollection::class, $this->location->getStartPaths());
+    }
+        
+    public function testShouldAddStartPath() {
+        $this->location->addStartPath($this->path);
+        
+        $this->assertNotEmpty($this->location->getStartPaths());
+    }
+    
+    public function testShouldRemoveStartPath() {
+        $this->location->addStartPath($this->path);
+        
+        $this->location->removeStartPath($this->path);
+        
+        $this->assertEmpty($this->location->getStartPaths());
+    }
+
+    public function testShouldSetEndPaths() {
+        $this->assertInstanceOf(ArrayCollection::class, $this->location->getEndPaths());
+    }
+    
+    public function testShouldAddEndPath() {
+        $this->location->addEndPath($this->path);
+        
+        $this->assertNotEmpty($this->location->getEndPaths());
+    }
+    
+    public function testShouldRemoveEndPath() {
+        $this->location->addEndPath($this->path);
+        
+        $this->location->removeEndPath($this->path);
+        
+        $this->assertEmpty($this->location->getEndPaths());
     }
 }
